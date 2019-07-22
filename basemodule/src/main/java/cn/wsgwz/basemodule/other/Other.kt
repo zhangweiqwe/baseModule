e6328a5f9e04ce5adc1789d6331bd9eac30eb8b0
Administrator <857899299@qq.com>
2019-07-22 09:10:12 +0800

    fix bug

---
+++ basemodule/src/main/java/cn/wsgwz/basemodule/other/Other.kt
@@ -0,0 +1,12 @@
+package cn.wsgwz.basemodule.other
+
+class Other {
+    /*
+        https://lbs.amap.com/api/amap-mobile/guide/android/route
+         http://lbsyun.baidu.com/index.php?title=uri/api/android
+         */
+
+
+
+
+}
---
+++ basemodule/src/main/java/cn/wsgwz/basemodule/other/Simple.java
@@ -0,0 +1,28 @@
+package cn.wsgwz.basemodule.other;
+
+public class Simple {
+
+    public void d(){
+
+    }
+
+    public static void main(String[] args){
+        new B().say();
+    }
+    interface A{
+        String str = null;
+        Simple b = new Simple();
+    }
+
+    static class B implements A{
+        public static void d() {
+            b.d();
+        }
+
+        String str = "";
+        private void say(){
+            str = "fsfsf";
+            System.out.println(str+"<-");
+        }
+    }
+}
