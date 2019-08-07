d18444928e967ce8d27dd74b157f2d58ae44f1c6
home user <1432818783@qq.com>
2019-08-07 17:44:03 +0800

    fix some bug

--- basemodule/src/main/java/cn/wsgwz/basemodule/utilities/retrofit/Code.kt
+++ basemodule/src/main/java/cn/wsgwz/basemodule/utilities/retrofit/Code.kt
@@ -1,20 +1,5 @@
 package cn.wsgwz.basemodule.utilities.retrofit
 
 enum class Code(val code:Int){
-    SUCCESS(0),FAILURE(1),EXCEPTION(-1),NO_LOGIN(2);
-    //SUCCESS(0),EXCEPTION(-1),BUSINESS_LOGIC_ERROR(1),NOT_LOGGED_IN(2)
-
-    //success(0, "操作成功"), failure(1, "操作失败"), exception(-1, "系统繁忙"), noLogin(2, "未登录");
-
-
-    companion object{
-        fun getCode(code:Int):Code?{
-            Code.values().forEach {
-                if(code==it.code){
-                    return it
-                }
-            }
-            return null
-        }
-    }
+    SUCCESS(0),NO_LOGIN(2),FAILURE(1),EXCEPTION(-1);
 }
